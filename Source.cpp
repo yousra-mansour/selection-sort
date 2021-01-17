4286a1ca4d6047f182cb2f52477758925f95a371
yousra-mansour <yousra.m.mansour@gmail.com>
2021-01-17 15:20:59 +0200

    Add Program to sort an array of strings using Selection Sort

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,40 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+#include<string>
+#define MAX_LEN 50 
+
 using namespace std;
 
 void selectionSort(int arr[], int Size);
 void swap(int *i, int *place);
 int print(int arr[], int Size);
+//void selectionSortTEXT(int arr[], int Size);
+int printTEXT(char arr[][MAX_LEN]);
+void selectionSortTEXT(char arr[][50], int n);
+
 
 
 int main()
 {
-	srand((unsigned) time(0));
+	/*srand((unsigned) time(0));
 	int arr[30];
 	for (int i = 0; i < 30; i++)
 	{
-		arr[i] = rand() % 50;
+		arr[i] = rand() % 30;
 	}
 	cout << "the array befor sorting  ";
 	print(arr, 30);
 	selectionSort(arr, 30);
 	cout  << "the array befor sorting  ";
-	print(arr, 30);
+	print(arr, 30);*/
 
-
+	char text[][MAX_LEN] = { "paper", "true", "soap" ,"floppy" ,"flower" };
+	printTEXT(text);
+	 int n = sizeof(text) / sizeof(text[0]);
+	 cout << n;
+	selectionSortTEXT(text, n);
+    printTEXT(text);
 	return 0;
 }
 
@@ -62,4 +74,43 @@ int print(int arr[], int Size)
 	}
 	cout << endl;
 	return 0;
+}
+int printTEXT(char arr[][MAX_LEN] )
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			cout << arr[i][j];
+		}
+		cout << "  ";
+	}
+	cout << endl;
+	return 0;
+}
+
+void selectionSortTEXT(char arr[][MAX_LEN], int n) {
+	int i, j, mIndex;
+	// Move boundary of unsorted subarray one by one
+	char minStr[MAX_LEN];
+	for (i = 0; i < n - 1; i++) {
+		// Determine minimum element in unsorted array
+		int mIndex = i;
+		strcpy(minStr, arr[i]);
+		for (j = i + 1; j < n; j++) {
+			// check whether the min is greater than arr[j]
+			if (strcmp(minStr, arr[j]) > 0) {
+				// Make arr[j] as minStr and update min_idx
+				strcpy(minStr, arr[j]);
+				mIndex = j;
+			}
+		}
+		// Swap the minimum with the first element
+		if (mIndex != i) {
+			char temp[MAX_LEN];
+			strcpy(temp, arr[i]); //swap item[pos] and item[i]
+			strcpy(arr[i], arr[mIndex]);
+			strcpy(arr[mIndex], temp);
+		}
+	}
 }
